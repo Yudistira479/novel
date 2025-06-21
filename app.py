a9d078cfc61e06954f887de04d98c12f5b3e077a
Yudistira479 <yudistirapra002@gmail.com>
2025-06-21 16:06:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.set_page_config(
 # --- Path dan URL ---
 MODEL_PATH = 'model_pelatihan.pkl'
 VECTORIZER_PATH = 'tfidf.pkl' # Pastikan nama file ini sesuai di GitHub Anda
-DATA_URL = 'https://raw.githubusercontent.com/Yudistira479/novel/main/novels.csv'
+DATA_URL = 'https://github.com/Yudistira479/novel/blob/main/novels.csv'
 # Atau, jika Anda menggunakan file lokal sebagai workaround:
 # DATA_PATH = 'novels.csv' 
 
